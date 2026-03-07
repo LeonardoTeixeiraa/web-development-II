2b6e99406e812c7324294b2b823d9d2863a9164b
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-03-07 15:57:51 -0300

    feat: implementar menu de exercícios

---
+++ Aula5/5a-loops/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Menu praticandos de loops</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Menu praticandos de loops</h1>
+    <ul class="list-group">
+        <li class="list-group-item"><a href="praticando-tabuada/praticando-tabuada.php">1. Praticando Tabuada</a></li>
+        <li class="list-group-item"><a href="praticando-contador/contador-form.php">2. Praticando Contador</a></li>
+        <li class="list-group-item"><a href="praticando-numprimos/praticando-numprimos.php">3. Praticando Números primos</a></li>
+        <li class="list-group-item"><a href="gerador-tabelas/tabela-form.php">4. Praticando Gerador de tabela</a></li>
+    </ul>
+</body>
+</html>
