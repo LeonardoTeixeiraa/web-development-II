bb2f1bf6d9b26c07fe7370d73d7fadf8e7bfbe93
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-04-16 07:48:11 -0300

    feat(index.php): implementa um formulário de login na página

---
+++ gerenciador-de-tarefas/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Login</title>
+</head>
+
+<body class="bg-light">
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="card shadow p-4" style="width: 100%; max-width: 400px;">
+            <h3 class="text-center mb-4">Login</h3>
+            <form action="" method="POST">
+                <div class="mb-3">
+                    <label for="" class="form-input">Usuário</label>
+                    <input type="text" name="usuario" class="form-control" required>
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-input">Senha</label>
+                    <input type="password" name="senha" class="form-control" required>
+                </div>
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-primary">Entrar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
