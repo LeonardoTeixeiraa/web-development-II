4c11a78e39d4cf93f3fefa8dccc021f6f081d1cd
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-04-16 20:18:19 -0300

    feat: implementar método para deletar uma tarefa pelo id

---
+++ gerenciador-de-tarefas/api/deletarTarefa.php
@@ -0,0 +1,15 @@
+<?php 
+//Recebe o id da requisição HTTP
+$data = json_decode(file_get_contents('php://input'));
+$id = $data['id'];
+
+//Receber os dados atuais da base de dados
+$tarefas = json_decode(file_get_contents('../tarefas.json'));
+
+//remover a tarefa do array sobrescrevendo
+$tarefas = array_filter($tarefas, fn($t) => $t['id'] !== $id);
+
+file_put_contents('../tarefas.json', json_encode($tarefas, JSON_PRETTY_PRINT));
+
+echo json_encode(['ok' => true]);
+?>
