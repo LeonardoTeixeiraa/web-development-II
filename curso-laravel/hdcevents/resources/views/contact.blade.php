4227784d5b13ffd71cbda2229e2be7d8c49d4855
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-05-21 22:58:41 -0300

    feat: implementar rotas e estrutura de condição na página

--- curso-laravel/hdcevents/resources/views/contact.blade.php
+++ curso-laravel/hdcevents/resources/views/contact.blade.php
@@ -1,2 +1,2 @@
 <h1>Página de contatos</h1>
-<a href="/">Voltar para Home</a>
+C
--- curso-laravel/hdcevents/routes/web.php
+++ curso-laravel/hdcevents/routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = 'Leonardo';
+    return view('welcome', ['nome' => $nome]);
 });
 
 Route::get('/contact', function () {
