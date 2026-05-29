31894b22e9c24f364fc01e1e84f745e40cd3f265
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-05-29 03:58:31 -0300

    feat: implementar Upload de imagem de capa para o evento

--- curso-laravel/hdcevents/resources/views/welcome.blade.php
+++ curso-laravel/hdcevents/resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
     <div id="cards-container" class="row">
         @foreach($events as $event)
         <div class="card col-md-3">
-            <img src="/img/event_placeholder.jpeg" alt="{{ $event->title}}">
+            <img src="/img/events/{{$event->image}}" alt="{{ $event->title}}">
             <div class="card-body">
                 <p class="card-date">{{ date('d/m/Y', strtotime($event->date)) }}</p>
                 <h5 class="card-title">{{ $event->title }}</h5>
