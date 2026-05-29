d82e0aceb48621899321a66ba596094a55feafae
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-05-29 04:24:56 -0300

    feat: aplicando estilos à página de saiba mais do evento

--- curso-laravel/hdcevents/resources/views/welcome.blade.php
+++ curso-laravel/hdcevents/resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
                 <p class="card-date">{{ date('d/m/Y', strtotime($event->date)) }}</p>
                 <h5 class="card-title">{{ $event->title }}</h5>
                 <p class="card-participantes">X participantes</p>
-                <a href="#" class="btn btn-primary">Saber Mais</a>
+                <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber Mais</a>
             </div>
         </div>
         @endforeach
